e6fd704bd4be4559ca29e030fbf793a57a79d64b
Sun Yi an <158817429+qza666@users.noreply.github.com>
2025-01-31 16:07:41 +0900

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,15 +5,17 @@ import (
 )
 
 type Config struct {
-	Port              int
-	CIDR              string
-	Bind              string
-	AutoRoute         bool
-	AutoForwarding    bool
-	AutoIpNoLocalBind bool
-	UseDOH            bool
-	Verbose           bool
-	AuthConfig        AuthConfig
+	RandomIPv6Port     int
+	RealIPv4Port       int
+	CIDR               string
+	Bind               string
+	AutoRoute          bool
+	AutoForwarding     bool
+	AutoIpNoLocalBind  bool
+	UseDOH             bool
+	Verbose            bool
+	AuthConfig         AuthConfig
+	RealIPv4           string
 }
 
 type AuthConfig struct {
@@ -23,16 +25,18 @@ type AuthConfig struct {
 
 func ParseFlags() *Config {
 	cfg := &Config{}
-	flag.IntVar(&cfg.Port, "port", 33300, "server port")
-	flag.StringVar(&cfg.CIDR, "cidr", "", "ipv6 cidr is required")
+	flag.IntVar(&cfg.RandomIPv6Port, "random-ipv6-port", 100, "Port for random IPv6 proxy")
+	flag.IntVar(&cfg.RealIPv4Port, "real-ipv4-port", 101, "Port for real IPv4 proxy")
+	flag.StringVar(&cfg.CIDR, "cidr", "", "IPv6 CIDR is required")
 	flag.StringVar(&cfg.AuthConfig.Username, "username", "", "Basic auth username")
 	flag.StringVar(&cfg.AuthConfig.Password, "password", "", "Basic auth password")
 	flag.StringVar(&cfg.Bind, "bind", "0.0.0.0", "Bind address")
 	flag.BoolVar(&cfg.AutoRoute, "auto-route", true, "Auto add route to local network")
-	flag.BoolVar(&cfg.AutoForwarding, "auto-forwarding", true, "Auto enable ipv6 forwarding")
-	flag.BoolVar(&cfg.AutoIpNoLocalBind, "auto-ip-nonlocal-bind", true, "Auto enable ipv6 non local bind")
+	flag.BoolVar(&cfg.AutoForwarding, "auto-forwarding", true, "Auto enable IPv6 forwarding")
+	flag.BoolVar(&cfg.AutoIpNoLocalBind, "auto-ip-nonlocal-bind", true, "Auto enable IPv6 non local bind")
 	flag.BoolVar(&cfg.UseDOH, "use-doh", true, "Use DNS over HTTPS instead of DNS over TLS")
 	flag.BoolVar(&cfg.Verbose, "verbose", false, "Enable verbose logging")
+	flag.StringVar(&cfg.RealIPv4, "real-ipv4", "", "Server's real IPv4 address")
 	flag.Parse()
 	return cfg
 }
